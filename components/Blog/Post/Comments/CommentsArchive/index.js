24d4a6a66941401548fbd9b61e33bb6c04a39742
Azhar Zaman <68331974+azharzaman1@users.noreply.github.com>
2022-07-29 20:25:14 +0500

    Minor fix

--- components/Blog/Post/Comments/CommentsArchive/index.js
+++ components/Blog/Post/Comments/CommentsArchive/index.js
@@ -8,7 +8,7 @@ const CommentsArchive = ({ comments, className }) => {
   return (
     <div className={`${className}`}>
       <Heading>Discussion</Heading>
-      {!comments && <Text className="mt-2">No comments</Text>}
+      {comments.length === 0 && <Text className="mt-2">No comments</Text>}
       {comments.length > 0 ? (
         <div className="comments-list flex flex-col mt-4">
           {comments.map((comment) => (
